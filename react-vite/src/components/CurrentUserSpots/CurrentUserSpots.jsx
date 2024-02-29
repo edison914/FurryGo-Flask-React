7ef942cf68988ed4efd3f10344c4045cefa78053
Zhongyuan Ding <zhongyuan.d@gmail.com>
2024-02-29 15:35:46 -0500

    finished added a delete a spot modal on my places.

--- react-vite/src/components/CurrentUserSpots/CurrentUserSpots.jsx
+++ react-vite/src/components/CurrentUserSpots/CurrentUserSpots.jsx
@@ -1,11 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCurrentUserSpotsThunk} from "../../redux/spots";
+import { getCurrentUserSpotsThunk } from "../../redux/spots";
 import SpotSimpleView from "../SpotSimpleView/SpotSimpleView";
+import DeleteASpot from "../DeleteASpotModal/DeleteASpotModal";
+import OpenModalButton from '../OpenModalButton/OpenModalButton';
+
 
 const CurrentUserSpots = () => {
   const dispatch = useDispatch();
-  const userId = useSelector((state) => state.session?.user?.id)
+  const userId = useSelector((state) => state.session?.user?.id);
   const spots = useSelector((state) => state.spots?.allSpots);
   // console.log(userId)
   useEffect(() => {
@@ -19,6 +22,17 @@ const CurrentUserSpots = () => {
         {spots?.map((spot) => (
           <div key={spot.id} className="SpotsContainerDiv">
             <SpotSimpleView spot={spot} />
+
+            {userId && userId === spot.user_id && (
+              <div className="comment-button-container">
+                <div className="comment-delete-button">
+                  <OpenModalButton
+                    modalComponent={<DeleteASpot spotId={spot.id} />}
+                    buttonText="Delete"
+                  />
+                </div>
+              </div>
+            )}
           </div>
         ))}
       </div>
